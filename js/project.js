5ceae2a3426bc16e54e289ca921691010d12a9dd
luisabaeta <luisabaeta@gmail.com>
2017-06-11 23:27:33 -0400

    Draft 2
    I decided to try a bit more. I made it look the way I wanted it to (at
    least I know how to do that!), and now I get it to do SOMETHING, but it
    doesn’t recognize the values inputted as numbers and I am very tired
    and I give up again. I can’t say I like javascript.

--- js/project.js
+++ js/project.js
@@ -2,21 +2,27 @@ var inputC;
 var inputF;
 
 // How do i get these values!?
-var inputC = document.getElementById("inputCtoF").value;
-var inputF = document.getElementById("inputFtoC").value;
+var inputC = document.getElementById("inputCtoF").input;
+var inputF = document.getElementById("inputFtoC").input;
+
+var inputC = Number(inputC);
+var inputF = Number(inputF);
 
 function convertFtoC() {
 	var outputC;
-	var outputC = (inputF - 32) * 0..5556;
+	var outputC = (inputF - 32) * 0.5556;
 	document.getElementById("solutionFtoC").innerHTML = outputC + " &deg;C";
 }
 
 function convertCtoF() {
 	var outputF;
 	var outputF = inputC * 1.8 + 32;
-	document.getElementById("solutionCtoF").innerHTML = inputC + " &deg;F";
-
+	document.getElementById("solutionCtoF").innerHTML = (inputC) + " &deg;F";
 }
 
 document.getElementById("convertCtoF").onclick = convertCtoF;
 document.getElementById("convertFtoC").onclick = convertFtoC;
+
+ if (inputC > 28) {
+		document.getElementById("icons").innerHTML = "hello";
+	}
